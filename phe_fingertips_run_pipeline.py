bec271257a98b4608785ede34a75601e0adf6102
Sally Sam <sally.sam@nihr.ac.uk>
2023-07-24 16:19:43 +0100

    initial commit

---
+++ phe_fingertips_run_pipeline.py
@@ -0,0 +1,9 @@
+from phe_fingertips_save_ref_files import save_all_ref_files
+
+# step 1
+## save the ref files from the phe fingertips website and from google sheets
+save_all_ref_files()
+
+# step 2
+
+
