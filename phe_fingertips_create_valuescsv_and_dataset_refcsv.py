773e8e74c41a4a1966a7b1eac2a0c5496ceb577c
Sally Sam <sally.sam@nihr.ac.uk>
2023-07-28 12:14:38 +0100

    filled in more of the pipeline

--- phe_fingertips_create_valuescsv_and_dataset_refcsv.py
+++ phe_fingertips_create_valuescsv_and_dataset_refcsv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # step 4
 ## clean concat values file
@@ -11,21 +12,82 @@ def remove_eng(dataframe:pd.DataFrame()):
     return dataframe.loc[dataframe.loc[:,'Area Type']!='England']
 
 
-# create values.csv
+def concat_str_cols(dataframe:pd.DataFrame(), cols:list, concat_str:str):
+    return dataframe.loc[:,cols].astype(str).agg(concat_str.join, axis=1)
 
-# create dataset_ref.csv
 
+def create_col_by_concat_cols(new_col_name:str, dataframe:pd.DataFrame(), cols:list, concat_str:str):
+    dataframe[new_col_name] = concat_str_cols(dataframe, cols, concat_str)
+    return dataframe
+
+
+def create_value_is_null_col(dataframe:pd.DataFrame()):
+    dataframe['value_is_null'] = dataframe.loc[:,'Value'].isna().astype(int)
+    return dataframe
+
+
+def replace_nulls_with_val(dataframe:pd.DataFrame(), col:str, null_val, replace_null_val):
+    dataframe[col] = dataframe.loc[:,col].replace(null_val, replace_null_val)
+    return dataframe
+
+
+replace_nulls_with_val(a, 'Value', np.nan, replace_null_val):
+
+
+
+valuescsv_required_cols = ['Area Code', 'Area Name', 'Indicator ID', 'Time period Sortable',
+                           'Sex', 'Age', 'area_type_id', 'Value', 'Count', 'Denominator',
+                           'Value Note']
+
+dataset_refcsv_required_cols = ['Dataset Downloaded Date', 'dataset_id', 'Indicator ID',
+                                'Time period', 'Time period Sortable', 'Time period range',
+                                'Sex', 'Age', 'area_type_id']
 
 
 def run(ref_files_date:str, values_download_date:str, remove_england:bool):
     values_folder_name = ref_files_date+'_values'
-    filepath = values_folder_name + '/' + values_download_date + '_values_concatenated.csv'
+    # filepath = values_folder_name + '/' + values_download_date + '_values_concatenated.csv'
+    filepath = values_folder_name + '/' + values_download_date + '_102_700to799.csv'
     data = pd.read_csv(filepath)
     
+    # 1 remove england
     if remove_england:
         data = remove_eng(data)
+        
+    # Restrict to columns of interest
     
-    return data
+    # create area_code
+    data = create_col_by_concat_cols('area_code', data, ['Area Code', 'area_type_id'], '_')
+    # create dataset_id
+    data = create_col_by_concat_cols('dataset_id', data, ['Indicator ID', 'Time period Sortable', 'Sex', 'Age', 'area_type_id'], '_')
+    # create value_is_null and replace nulls with null value
+    data = create_value_is_null_col(data)
+    
+    # rename cols
+    rename_cols_dict = {'Time period': 'Time Period',
+                        'Time period Sortable': 'Time Period Sortable',
+                        'Time period range': 'Time Period Range',
+                        'Value note': 'Value Note'}
+    data = data.rename(columns=rename_cols_dict)
+    
+    # create dataset_ref.csv
+    dataset_cols = ['Dataset Downloaded Date', 'dataset_id', 'Indicator ID', 'Time Period', 'Time Period Sortable', 'Time Period Range', 'Sex', 'Age', 'area_type_id']
+    dataset_cols_dict = {'area_type_id': '~area_type_id'}
+    dataset_ref = data.loc[:,dataset_cols].rename(columns=dataset_cols_dict).drop_duplicates()
+    
+    # create values.csv
+    values_cols = ['area_code', 'Area Name', 'dataset_id', 'Value', 'Count', 'Denominator',
+                   'value_is_null', 'Value Note']
+    values = data.loc[:,values_cols]
+
+    return data, dataset_ref, values
         
 
-a = run('2023-07-24', '2023-07-28', True)
+a,b,c = run('2023-07-24', '2023-07-28', True)
+
+c.head()
+a.columns
+
+(a.loc[:,'Value']==0).sum()
+
+a.loc[4032]
