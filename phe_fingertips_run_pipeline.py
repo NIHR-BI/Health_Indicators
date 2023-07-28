1330fb2e77dfe25e31f0964aa430f02f01338da4
Sally Sam <sally.sam@nihr.ac.uk>
2023-07-28 09:56:03 +0100

    amend step 2 to include chosen area types

--- phe_fingertips_run_pipeline.py
+++ phe_fingertips_run_pipeline.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 from phe_fingertips_save_ref_files import save_all_ref_files
-from phe_fingertips_save_values import save_values_for_all_ind_area_combos
+from phe_fingertips_save_values import save_values_choose_areas
 from phe_fingertips_concat_values import concat_files_in_folder_and_save
 
 
@@ -8,12 +10,16 @@ str_date='2023-07-24'
 
 # step 1
 ## save the ref files from the phe fingertips website and from google sheets
-save_all_ref_files()
+# save_all_ref_files()
 
 
 # step 2
-## save values for all indicator and area type combos
-save_values_for_all_ind_area_combos(str_date=str_date)
+## save values for chosen area types
+chosen_area_ids = pd.read_csv(str_date + '_area_hierarchy.csv', usecols=['AreaTypeId']).values.tolist()
+chosen_area_ids = [i for sublist in chosen_area_ids for i in sublist]
+folder_name = str_date + '_values/'
+
+save_values_choose_areas(str_date=str_date, areaids=chosen_area_ids, folder_name=folder_name)
 
 
 # step 3
