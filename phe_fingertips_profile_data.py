63fabdf088ce7b0fdb926d6a4d1761ed6977888f
Sally Sam <sally.sam@nihr.ac.uk>
2023-08-18 13:03:45 +0100

    changed to functions

--- phe_fingertips_profile_data.py
+++ phe_fingertips_profile_data.py
@@ -1,18 +1,35 @@
 import pandas as pd
+
 profile = pd.read_json('https://fingertips.phe.org.uk/api/profiles')
+
+# see all names
+profile.loc[:,['Id', 'Name']]
+
+def search_profile_names(profile, search_term:str):
+    return profile.loc[profile.loc[:,'Name'].str.contains(search_term, case=False)]
+
+search_profile_names(profile, search_term='wider')
+
 # 'perinatal-mental-health', 102
+# wider determinants of health 130
+# palliative 95
 
-perinatal_ids = pd.read_json('https://fingertips.phe.org.uk/api/indicator_metadata/by_profile_id?profile_id=102')
-len(perinatal_ids.columns)
+def return_ind_ids_for_profile_id(profile_id:int):
+    ind_ids = pd.read_json('https://fingertips.phe.org.uk/api/indicator_metadata/by_profile_id?profile_id=' + str(profile_id))
+    return list(ind_ids.columns)
 
-# # filter to get the dataset_ids
-dataset_ref = pd.read_csv('2023-08-18_dataset_ref.csv')
-perinatal_dataset_ref = dataset_ref.loc[dataset_ref.loc[:,'Indicator ID'].isin(perinatal_ids.columns)]
+def return_values_for_ind_ids(ind_ids:list, ref_files_date:str):
+    all_values = pd.read_csv(f"{ref_files_date}_values.csv")
+    values = all_values.loc[all_values.loc[:,'~Indicator ID'].isin(ind_ids)]
+    return values
 
-# filter the values for only the dataset_ids of interest
-values = pd.read_csv('2023-08-18_values.csv')
-perinatal_values = pd.merge(perinatal_dataset_ref.loc[:,'indicator_dataset_id'],
-                            values,
-                            how='inner')
+def save_values_for_profile_id(short_profile_name:str, profile_id:int, ref_files_date:str):
+    ind_ids = return_ind_ids_for_profile_id(profile_id)
+    values = return_values_for_ind_ids(ind_ids, ref_files_date)
+    
+    file_name = f"{ref_files_date}_values_{short_profile_name}.csv"
+    values.to_csv(file_name, index=False)
+    print(f"{file_name} successfully saved")
+    
 
-perinatal_values.to_csv('2023-08-18_values_perinatal.csv', index=False)
+save_values_for_profile_id(short_profile_name='palliative_end_of_life_care', profile_id=95, ref_files_date='2023-08-18')
