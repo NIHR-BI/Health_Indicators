39ca77e4e6b77a76390dd34d6e03ec2224a6b189
Sally Sam <sally.sam@nihr.ac.uk>
2023-08-18 09:59:34 +0100

    fix col names with gsheet change

--- phe_fingertips_save_csv_area_type_ref.py
+++ phe_fingertips_save_csv_area_type_ref.py
@@ -16,7 +16,7 @@ def save_area_type_ref_csv(ref_files_date):
                                          'Sequence', 'Indicator Information Downloaded Date']
                                 )
                           .replace(class_dict)
-        )
+        )    
     
     sequence_dict = {0: 'Not applicable'}
     areatypeid_ref['Sequence'] = areatypeid_ref.loc[:,'Sequence'].replace(sequence_dict)
@@ -24,27 +24,22 @@ def save_area_type_ref_csv(ref_files_date):
     areatypeid_ref['Class'] = areatypeid_ref.loc[:,'Class'].fillna(areatypeid_ref.loc[:,'Short'])
     
     area_hierarchy = pd.read_csv(ref_files_date + '_area_hierarchy.csv',
-                                 usecols=['AreaTypeId', 'map_id', 'hierarchy']
+                                 usecols=['area_type_id', 'map_id', 'map_id_hierarchy']
     )
     
-
     area_type_ref = (pd.merge(area_hierarchy, areatypeid_ref,
                      how='left',
-                     left_on='AreaTypeId', right_on='Id')
+                     left_on='area_type_id', right_on='Id')
              .drop(columns='Id')
     )
-    
-    col_dict = {'AreaTypeId': 'area_type_id',
-                'hierarchy': 'map_id_hierarchy',
-                'Name': 'Area Type Name',
-                'Short': 'Area Type Short Name',
-                'Class': 'Area Type Class',
-                'Sequence': ' Area Type Year Configuration'}
-    
-    area_type_ref = area_type_ref.rename(columns=col_dict)
     
     area_type_ref.to_csv(ref_files_date + '_area_type_ref.csv', index=False)
     print(ref_files_date + '_area_type_ref.csv successfully saved')
     
     # return area_type_ref
 
+
+if __name__ == '__main__':
+    ref_files_date = '2023-08-18'
+    test = save_area_type_ref_csv(ref_files_date)
+    
