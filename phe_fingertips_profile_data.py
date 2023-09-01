6a272e0ce123458bb363831aa9946699cc7b81f8
Sally Sam <sally.sam@nihr.ac.uk>
2023-09-01 15:54:53 +0100

    save values for all ind ids in a profile

--- phe_fingertips_profile_data.py
+++ phe_fingertips_profile_data.py
@@ -9,12 +9,12 @@ profile.loc[:,['Id', 'Name']]
 profile = profile[profile.loc[:,'HasAnyData']==True].reset_index(drop=True)
 
 # list all group names inside their profile names
-for profile_index in range(len(profile)):
-    name = profile.loc[:,'Name'][profile_index]
-    print(f"--------PROFILE: {name}")    
-    group = profile.GroupMetadata.to_dict()[profile_index]
-    for group_index in group:
-        print(dict(group_index)['Name'])
+# for profile_index in range(len(profile)):
+#     name = profile.loc[:,'Name'][profile_index]
+#     print(f"--------PROFILE: {name}")    
+#     group = profile.GroupMetadata.to_dict()[profile_index]
+#     for group_index in group:
+#         print(dict(group_index)['Name'])
 
 def search_profile_names(profile, search_term:str):
     return profile.loc[profile.loc[:,'Name'].str.contains(search_term, case=False)]
@@ -29,26 +29,32 @@ def return_ind_ids_for_profile_id(profile_id:int):
     ind_ids = pd.read_json('https://fingertips.phe.org.uk/api/indicator_metadata/by_profile_id?profile_id=' + str(profile_id))
     return list(ind_ids.columns)
 
-def return_values_for_ind_ids(ind_ids:list, ref_files_date:str):
-    all_values = pd.read_csv(f"{ref_files_date}_values.csv")
+def return_values_for_ind_ids(all_values, ind_ids:list, ref_files_date:str):
     values = all_values.loc[all_values.loc[:,'~Indicator ID'].isin(ind_ids)]
     return values
 
-def save_values_for_profile_id(short_profile_name:str, profile_id:int, ref_files_date:str):
+def save_values_for_profile_id(all_values, short_profile_name:str, profile_id:int, ref_files_date:str):
     ind_ids = return_ind_ids_for_profile_id(profile_id)
-    values = return_values_for_ind_ids(ind_ids, ref_files_date)
+    values = return_values_for_ind_ids(all_values, ind_ids, ref_files_date)
     
-    file_name = f"{ref_files_date}_values_{short_profile_name}.csv"
+    file_name = f"{ref_files_date}_values_for_profiles/{ref_files_date}_values_{short_profile_name}.csv"
     values.to_csv(file_name, index=False)
     print(f"{file_name} successfully saved")
     
 
-save_values_for_profile_id(short_profile_name='musculoskeletal_health', profile_id=76, ref_files_date='2023-08-18')
+
+ref_files_date='2023-08-18'
+all_values = pd.read_csv(f"{ref_files_date}_values.csv")
+
+for i, profile_id in enumerate(profile.loc[:,'Id']):
+    short_profile_name = profile.loc[i,'Key']
+    save_values_for_profile_id(all_values=all_values, short_profile_name=short_profile_name,
+                            profile_id=profile_id, ref_files_date=ref_files_date)
 
 
 
-import pandas as pd
+# import pandas as pd
 
-a = pd.read_csv('2023-08-18_indicator_ref.csv')
-len(a)
-a.Indicator.nunique()
+# a = pd.read_csv('2023-08-18_indicator_ref.csv')
+# len(a)
+# a.Indicator.nunique()
